2640854abd27909ba7d2cd222ea17fab6fa1f384
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-10-27 23:33:16 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,17 @@ class User < ApplicationRecord
   has_many :items
   # has_many :purchase_records
 
-  validates :nickname, presence: true, length: { maximum: 6 }
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :nickname, presence: true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end
   validates :birth_day, presence: true
   
 
