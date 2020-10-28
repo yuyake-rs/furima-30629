644801f941c89b3ba4571d519c447fb9d9a500a7
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-10-28 15:06:42 +0900

    不要なコメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   # has_many :purchase_records
 
   validates :nickname, presence: true
-  # validates :email, unique: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
