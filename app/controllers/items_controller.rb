52e1c605330aaf8bd22c7fb64c41e594c5f74e0a
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-11-03 18:19:56 +0900

    items/newのルーティング/ビューの配置/ログインしてないときの画面遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,16 @@
 class ItemsController < ApplicationController
 	def index
-		@item = Item.order("created_at DESC")
+		# @item = Item.order("created_at DESC")
 	end
+
+	def new
+	end
+
+
 	
 	private
 
-  def message_params
-    params.require(:item).permit(:name, :information, :category_id, :condition_id, :delivery_fee_id, :ship_from_location_id, :delivery_start_id, :price :image).merge(user_id: current_user.id)
-  end
+  #def message_params
+    #params.require(:item).permit(:name, :information, :category_id, :condition_id, :delivery_fee_id, :ship_from_location_id, :delivery_start_id, :price :image).merge(user_id: current_user.id)
+  #end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index]
+  resources :items, only: [:index, :new]
   resources :users, only: [:new, :edit, :update]
 end
