2956b9c3afb8a425c70ab3d198f62544a962a255
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-11-03 17:37:19 +0900

    item modelのvalidation設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,11 @@ class Item < ApplicationRecord
 	belongs_to :delivery_fee
 	belongs_to :ship_from_location
 	belongs_to :delivery_start
+
+	validates :name, :information, :category, :condition, :delivery_fee, :ship_from_location, :delivery_start, :image, presence: true
+	validates :name, length: { maxmum: 40}
+	validates :information, length: { maxmum: 1_000}
+	validates :category_id, :condition_id, :delivery_fee_id, :ship_from_location_id, :delivery_start_id, numericality: { other_than: 0 } 
+	validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
 end
