9c93e12d90ba21745b56db0f9cd65e449aaef6ff
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-11-11 08:23:32 +0900

    editアクションのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show]
+  resources :items, only: [:index, :new, :create, :show, :edit]
   resources :users, only: [:new, :edit, :update]
 end
