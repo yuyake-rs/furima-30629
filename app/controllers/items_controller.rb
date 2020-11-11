17cf76622edcf852b61ea41f14de18cbcf0f3f57
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-11-11 12:04:01 +0900

    destroyアクションのエラー時の条件切り分けと、重複部分の切り出し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -34,9 +34,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :edit
   end
 
   private
