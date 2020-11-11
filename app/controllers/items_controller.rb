c7c191cf43f410af8d7533f681dc659b565d55bf
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-11-11 12:56:22 +0900

    ログインしているユーザーが出品者の場合のみ削除できるという機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :destroy_item, only: [:destroy]
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -49,4 +50,11 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def destroy_item
+    unless  user_signed_in? && current_user.id == @item.user_id
+      render root_path
+    end
+  end
+  
 end
