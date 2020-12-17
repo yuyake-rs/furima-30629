432fd56e2b1e1ea2068180fedee4c482e0dc19c1
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-12-17 12:17:01 +0900

    routing newの削除、phonnumber 12桁ギリギリのテスト、card.jsのインデント修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,13 +20,13 @@ const pay = () => {
         const renderDom = document.getElementById("charge-form");
         const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
-			}
-			document.getElementById("card-number").removeAttribute("name");
+      }
+      document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
       document.getElementById("card-exp-month").removeAttribute("name");
-			document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
 
-			document.getElementById("charge-form").submit();
+      document.getElementById("charge-form").submit();
     });
   });
 };
